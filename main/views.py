c51ded8a71871ff1aceec01c64f1dd8824faea57
Asel <adelyakalenova@gmail.com>
2022-10-24 16:21:37 +0600

    hw1

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime
+
+def about_us(request):
+    return render(request, "about_us.html")
+
+
+
+def date_now(request):
+    a = datetime.datetime.now()
+    return render(request, "date_now.html", {'dat':a})
+
